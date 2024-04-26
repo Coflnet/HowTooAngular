f5a15281acd1fa88f836f20731ef3bba0a725736
Äkwav <ekwav@coflent.com>
2024-04-26 22:01:09 +0200

    generate stub components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'tutorial/:id', loadComponent: () => import('./tutorial/tutorial.component').then(mod => mod.TutorialComponent) },
+    { path: 'tutorials', loadComponent: () => import('./full-list/full-list.component').then(mod => mod.FullListComponent) },
+    { path: '', redirectTo: 'tutorials', pathMatch: "full" }
+];
---
+++ src/app/full-list/full-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-full-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './full-list.component.html',
+  styleUrl: './full-list.component.scss'
+})
+export class FullListComponent {
+
+}
