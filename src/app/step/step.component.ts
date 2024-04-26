d0dda3162a482b89adfa8cfd5cb5a81bff48a349
Äkwav <ekwav@coflent.com>
2024-04-27 00:07:36 +0200

    tutorial edit poc

--- src/app/step/step.component.ts
+++ src/app/step/step.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TutorialsTable } from '../client';
+import { Editor, NgxEditorModule } from 'ngx-editor';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-step',
   standalone: true,
-  imports: [],
+  imports: [NgxEditorModule,FormsModule,CommonModule,MatIconModule,MatButtonModule],
   templateUrl: './step.component.html',
   styleUrl: './step.component.scss'
 })
 export class StepComponent {
+  @Input()
+  step: TutorialsTable = null!;
+  @Output()
+  stepChanged = new EventEmitter<TutorialsTable>();
+  editor: Editor|undefined;
+  html = '';
 
+  edit(click:MouseEvent) {
+    click.stopPropagation();
+    this.editor = new Editor();
+    this.html = this.step.text || '';
+    this.editor.view.focus();
+    
+  }
+
+  save() {
+    if (this.editor) {
+      this.step.text =  this.html;
+      this.stepChanged.emit(this.step);
+      this.editor.destroy();
+      this.editor = undefined;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.editor?.destroy();
+  }
 }
--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { TutorialsTable } from '../client';
+import { StepComponent } from '../step/step.component';
+import { CommonModule } from '@angular/common';
+import { NgxSortableModule } from 'ngx-sortable';
+import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-tutorial',
   standalone: true,
-  imports: [],
+  imports: [StepComponent, CommonModule, NgxSortableModule, CdkDropList, CdkDrag],
   templateUrl: './tutorial.component.html',
   styleUrl: './tutorial.component.scss'
 })
 export class TutorialComponent {
+  steps: TutorialsTable[] = [
+    { image_path: "https://coflnet.com/static/sky-image.png", text: "Step 1" },
+    { image_path: "https://coflnet.com/static/sky-image.png", text: "Step 2" },
+  ] as TutorialsTable[];
 
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.steps, event.previousIndex, event.currentIndex);
+  }
+  stepChanged() {
+    throw new Error('Method not implemented.');
+  }
 }
